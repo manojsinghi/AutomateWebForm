456663b6d2ca359e4bd1ffb0146588da9e730a7d
manojsinghi16 <manoj.singhi@onecode.in>
2022-07-26 11:04:52 +0530

    added docker config for deployig on EBS

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -4,9 +4,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    String hello() {
-        return "Hello World, Spring Boot!";
-    }
-
 }
--- src/main/java/MyWebApplication.java
+++ src/main/java/MyWebApplication.java
@@ -1,9 +1,16 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyWebApplication {
 
+    @RequestMapping("/welcome")
+    String hello() {
+        return "Hello World, Spring Boot!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyWebApplication.class, args);
